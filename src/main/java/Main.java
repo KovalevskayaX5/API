4de4ae32d5d0649127ae6462ad4c85ad47e72083
YanaK <Yana.Koval15@yandex.ru>
2023-02-25 13:26:45 +0300

    main

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Яна";
+        post.surname = "Ковалевская";
+        post.patronymic = "Михайловна";
+        post.passport = "1956 № 126985";
+        post.phone = "+7 956 458 96 63";
+        post.subscription = "true";
+        post.birthday = new FormDate();
+        post.birthday.day = 20;
+        post.birthday.month = 12;
+        post.birthday.year = 1996;
+    }
+}
